5e28933e1da1448bdb1dd9f682e17d0f514d03f6
Brad Bowie <brad.bowie@nextraq.com>
2015-09-28 10:30:02 -0400

    adding broken unit test

---
+++ src/test/java/us/broani/sup/MainTest.java
@@ -0,0 +1,14 @@
+package us.broani.sup;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void helloWorld() {
+		Assert.assertTrue(false);
+	}
+
+}
