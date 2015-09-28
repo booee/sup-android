1b8c5ddc8b43e7e079cc3c591b9624f693ff5803
Brad Bowie <brad.bowie@nextraq.com>
2015-09-28 09:53:47 -0400

    fixing the build back

--- src/main/java/us/broani/sup/ui/MainActivity.java
+++ src/main/java/us/broani/sup/ui/MainActivity.java
@@ -11,7 +11,5 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		BREAK THE BUILD!!!111
 	}
 }
